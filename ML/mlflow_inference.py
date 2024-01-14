50a5c89196404a6025daad883614938283e8aa08
junghoon.lee <vhrehfdl7tp@naver.com>
2024-01-14 12:14:25 +0900

    Delete mlruns

--- ML/mlflow_inference.py
+++ ML/mlflow_inference.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 if __name__ == '__main__':
-    logged_model = 'runs:/72558aa709ab4d898c54c8b6f2a2d2ff/titanic_model'
+    logged_model = 'runs:/bfb40ff99d664f0aad48ba188f86f7ba/titanic_model'
 
     loaded_model = mlflow.pyfunc.load_model(logged_model)
     test_x = pd.DataFrame({"Pclass": [2, 1], "Sex": [0, 1], "Fare": [3.3211, 3.3211], "SibSp": [3, 3], "Parch":[3, 3]})
